727afbe6112b8c898fcae88ce4900fe7f5dfd39a
Towfik <tufikhasan05@gmail.com>
2022-04-20 01:41:54 +0600

    demo blogs  data increase

--- src/demoData.js
+++ src/demoData.js
@@ -4,13 +4,13 @@ export const bloglist = [
     category: 'development',
     subCategory: ['frontend', 'ui-ux', 'design'],
     title:
-      'Block and Blockstream are partnering with Tesla on an off-grid, solar-powered Bitcoin mine in Texas',
+      'development Block and Blockstream are partnering with Tesla on an off-grid, solar-powered Bitcoin mine in Texas',
     description:
-      'Block and Blockstream are partnering with Tesla on an open-source, solar-powered Bitcoin mine, the companies announced Friday. Tesla’s 3.8-megawatt Solar PV array and its 12 megawatt-hour Megapack will power the facility, and construction has started on the p…',
+      'development Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of Block and Blockstream are partnering with Tesla on an open-source, solar-powered Bitcoin mine, the companies announced Friday. Tesla’s 3.8-megawatt Solar PV array and its 12 megawatt-hour Megapack will power the facility, and construction has started on the p…',
     imageUrl:
-      'https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg',
+      'https://previews.123rf.com/images/vladgrin/vladgrin1809/vladgrin180900072/110084722-programming-web-banner-best-programming-languages-technology-process-of-software-development.jpg',
     publishedAt: '2022-04-08T16:02:52Z',
-    authorName: 'Jay Peters',
+    authorName: 'Ian Doyle',
     authorImg:
       'https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-PNG-Free-Image.png',
   },
@@ -18,13 +18,14 @@ export const bloglist = [
     id: 2,
     category: 'backend',
     subCategory: ['node', 'nextjs', 'laravel'],
-    title: 'Bitcoin Reaches Milestone ',
+    title:
+      'backend Joe Biden arrives in from a Lorem Ipsum passage, and going through the cites of - The Guardian',
     description:
-      'Block and Blockstream are partnering with Tesla on an open-source, solar-powered Bitcoin mine, the companies announced Friday. Tesla’s ',
+      'New Hampshire to promote $1tn infrastructure law live Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, Block and Blockstream are partnering with Tesla on an open-source, solar-powered Bitcoin mine, the companies announced Friday. Tesla’s ',
     imageUrl:
-      'https://cdn.pixabay.com/photo/2014/02/27/16/10/tree-276014__340.jpg',
+      'https://www.datocms-assets.com/14946/1590690720-frontend-and-backend-frameworks.png',
     publishedAt: '2022-04-08T16:02:52Z',
-    authorName: 'John',
+    authorName: 'TMZ Staff',
     authorImg:
       'https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-PNG-Free-Image.png',
   },
@@ -35,10 +36,11 @@ export const bloglist = [
     title:
       'Fullstack Blockstream are partnering with Tesla on an off-grid, solar-powered Bitcoin mine in Texas',
     description:
-      'Block and Blockstream are partnering with Tesla on an open-source, solar-powered Bitcoin mine, the companies announced Friday. Tesla’s 3.8-megawatt Solar PV array and its 12 megawatt-hour Megapack will power the facility, and construction has started on the p…',
+      'Fullstack There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isnt anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary,Block and Blockstream are partnering with Tesla on an open-source, solar-powered Bitcoin mine, the companies announced Friday. Tesla’s 3.8-megawatt Solar PV array and its 12 megawatt-hour Megapack will power the facility, and construction has started on the p…',
+    publishedAt: '2022-04-19T18:28:25Z',
     imageUrl:
-      'https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__480.jpg',
-    authorName: 'Willim Peters',
+      'https://res.cloudinary.com/dmsxwwfb5/image/upload/v1595866967/full-stack-devlopment-min.png',
+    authorName: 'Jill Lawless',
     authorImg:
       'https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-PNG-Free-Image.png',
   },
@@ -47,11 +49,102 @@ export const bloglist = [
     category: 'wordpress',
     subCategory: ['php', 'mysql', 'laravel'],
     title:
-      'Fullstack Blockstream are partnering with Tesla on an off-grid, solar-powered Bitcoin mine in Texas',
+      'wordpress Liverpool vs Manchester United - LIVE updates - Liverpool Echo',
     description:
-      'Block and Blockstream are partnering with Tesla on an open-source, solar-powered Bitcoin mine, the companies announced Friday. Tesla’s 3.8-megawatt Solar PV array and its 12 megawatt-hour Megapack will power the facility, and construction has started on the p…',
-    imageUrl: 'https://cdn.wallpapersafari.com/95/34/cEX23Y.jpg',
+      'wordpress Bt only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum and Blockstream are partnering with Tesla on an open-source, solar-powered Bitcoin mine, the companies announced Friday. Tesla’s 3.8-megawatt Solar PV array and its 12 megawatt-hour Megapack will power the facility, and construction has started on the p…',
+    publishedAt: '2022-04-19T18:28:25Z',
+    imageUrl:
+      'https://tortoiseandharesoftware.com/wp-content/uploads/2020/09/WordPress-Design-Considerations.jpg',
+    authorName: 'Ron Amadeo',
+    authorImg:
+      'https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-PNG-Free-Image.png',
+  },
+  {
+    id: 5,
+    category: 'frontend',
+    subCategory: ['react', 'ui-ux', 'design'],
+    title:
+      'frontend Blac Chynas PageMaker including versions of Lorem Ipsumm, Tokyo Toni, Goes on Rant Against Kardashians, Must Stay Away From Court Hearings',
+    description:
+      'Mot only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus Block and Blockstream are partnering with Tesla on an open-source, solar-powered Bitcoin mine, the companies announced Friday. Tesla’s 3.8-megawatt Solar PV array and its 12 megawatt-hour Megapack will power the facility, and construction has started on the p…',
+    imageUrl:
+      'https://i.pinimg.com/736x/3d/6d/68/3d6d685fc7fa3f7cd99b9b9e0bfb151d.jpg',
+    publishedAt: '2022-04-08T16:02:52Z',
+    authorName: 'Jay Peters',
+    authorImg:
+      'https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-PNG-Free-Image.png',
+  },
+  {
+    id: 6,
+    category: 'bootstrap',
+    subCategory: ['node', 'nextjs', 'laravel'],
+    title:
+      'bootstrap Florida Gov. DeSantis Seeks to End Walt Disney World’s Special Tax District - The Wall Street Journal',
+    description:
+      'bootstrap ant only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsumd Blockstream are partnering with Tesla on an open-source, solar-powered Bitcoin mine, the companies announced Friday. Tesla’s ',
+    imageUrl:
+      'https://bootstrapmade.com/content/templatefiles/OnePage/OnePage-bootstrap-website-template-md.png',
+    publishedAt: '2022-04-08T16:02:52Z',
+    authorName: 'Mike Householder',
+    authorImg:
+      'https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-PNG-Free-Image.png',
+  },
+  {
+    id: 7,
+    category: 'portfolio',
+    subCategory: ['react', 'mern', 'express'],
+    title:
+      'portfolio Johnson says sorry for partygate as critics prep censure bid - The Associated Press - en Español',
+    description:
+      'portfolio solar-powered Bitcoin mine, the companies announced Friday. Tesla’s 3.8-megawatt Solar PV array and its 12 megawatt-hour Megapack will power the facility, and construction has started on the Block and Blockstream are partnering with Tesla on an open-source, solar-powered Bitcoin mine, the companies announced Friday. Tesla’s 3.8-megawatt Solar PV array and its 12 megawatt-hour Megapack will power the facility, and construction has started on the p…',
+    publishedAt: '2022-04-19T18:28:25Z',
+    imageUrl: 'https://i.ytimg.com/vi/AKNvTxWOdKw/maxresdefault.jpg',
+    authorName: 'Josh Kosman',
+    authorImg:
+      'https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-PNG-Free-Image.png',
+  },
+  {
+    id: 8,
+    category: 'blogger',
+    subCategory: ['php', 'mysql', 'laravel'],
+    title:
+      'blogger OnePlus announces the Nord N20: $282 price, 18-month-old OS - Ars Technica',
+    description:
+      'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isnt anything Block and Blockstream are partnering with Tesla on an open-source, solar-powered Bitcoin mine, the companies announced Friday. Tesla’s 3.8-megawatt Solar PV array and its 12 megawatt-hour Megapack will power the facility, and construction has started on the p…',
+    publishedAt: '2022-04-19T18:28:25Z',
+    imageUrl:
+      'https://mllj2j8xvfl0.i.optimole.com/Lsv2lkg.cHDL~36fa1/w:auto/h:auto/q:98/https://s15165.pcdn.co/wp-content/uploads/2018/05/linkedin.jpg',
     authorName: 'Willim Peters',
+    authorImg:
+      'https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-PNG-Free-Image.png',
+  },
+  {
+    id: 9,
+    category: 'realstate',
+    subCategory: ['react', 'mern', 'express'],
+    title:
+      'realstate Blockstream are partnering with Tesla on an off-grid, solar-powered Bitcoin mine in Texas',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries Block and Blockstream are partnering with Tesla on an open-source, solar-powered Bitcoin mine, the companies announced Friday. Tesla’s 3.8-megawatt Solar PV array and its 12 megawatt-hour Megapack will power the facility, and construction has started on the p…',
+    publishedAt: '2022-04-19T18:28:25Z',
+    imageUrl:
+      'https://cdn.dribbble.com/users/2947819/screenshots/16603527/media/62b848c79595198eca1bb9cdd4922780.png?compress=1&resize=400x300',
+    authorName: 'Tina Burnside',
+    authorImg:
+      'https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-PNG-Free-Image.png',
+  },
+  {
+    id: 10,
+    category: 'ecommerce',
+    subCategory: ['php', 'mysql', 'laravel'],
+    title:
+      'ecommerce Liverpool vs Manchester United - LIVE updates - Liverpool Echo',
+    description:
+      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page Block and Blockstream are partnering with Tesla on an open-source, solar-powered Bitcoin mine, the companies announced Friday. Tesla’s 3.8-megawatt Solar PV array and its 12 megawatt-hour Megapack will power the facility, and construction has started on the p…',
+    publishedAt: '2022-04-19T18:28:25Z',
+    imageUrl:
+      'https://cdn.cyberchimps.com/wp-content/uploads/2021/04/The-Future-of-Commerce-Is-Yours-BigCommerce-1-1024x542.jpg',
+    authorName: 'Tim MacMahon"',
     authorImg:
       'https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-PNG-Free-Image.png',
   },
