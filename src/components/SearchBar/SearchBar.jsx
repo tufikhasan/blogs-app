4193536af6cd3d92e706c919a43819a117dab96b
Towfik <tufikhasan05@gmail.com>
2022-04-19 12:28:33 +0600

    search bar design completed

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './SearchBar.scss';
+
+const SearchBar = ({ formSubmit, value, handleSearchKey, clearSearch }) => {
+  return (
+    <div className="app__searchbar app__flex">
+      <form onSubmit={formSubmit}>
+        <input
+          className="p-text"
+          type="text"
+          value={value}
+          placeholder="Search by category"
+          onChange={handleSearchKey}
+        />
+        {<span onClick={clearSearch}>X</span>}
+        <button className="primary_button">Go</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import './Home.scss';
+import { SearchBar } from '../../components';
+
 const Home = () => {
-  return <div>Home</div>;
+  return <SearchBar />;
 };
 
 export default Home;
